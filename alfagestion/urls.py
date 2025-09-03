e841f418589e87b2bcd59e5a3ba6fe3adf3f0217
Zapata <zaoatah63@gmail.com>
2025-09-03 12:59:42 -0500

    caminos divididos

--- alfagestion/urls.py
+++ alfagestion/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('institutions.urls')),
-    # path('', include('students.urls')),
+    path('students/', include('students.urls', namespace='students')),
+    path('teachers/', include('teachers.urls', namespace='teachers')),
+
     # path('', include('teachers.urls')),
     # path('', include('evidence.urls')),
     # path('', include('reports.urls')),
