581d1ff6e578c486504554f71a71028697ee7330
Alejo <alejobedoyam2008@gmail.com>
2025-09-16 16:57:31 -0500

    backend cagado

--- alfagestion/settings.py
+++ alfagestion/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -87,7 +88,7 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": "1234",
         "HOST": "127.0.0.1",
-        "PORT": "5433",
+        "PORT": "5432",
     }
 }
 
